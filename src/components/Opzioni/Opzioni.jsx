2303b2c78cfed42aa4ea51273e76c8c307c0c76a
davide <davidemastronardi92@gmail.com>
2023-07-11 11:24:09 +0200

    illuminazione esterna completato

--- src/components/Opzioni/Opzioni.jsx
+++ src/components/Opzioni/Opzioni.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SiDesideraLaMessaTerra from "./SiDesideraLaMessaTerra";
+import IlluminazioneEsterna from './IlluminazioneEsterna';
 
 const Opzioni = () => {
   return (
     <div className="flex flex-col gap-4">
             <div className="titolo-sezione">OPZIONI</div>
             <SiDesideraLaMessaTerra/>
+            <IlluminazioneEsterna/>
         </div>
   )
 }
